001bfb26b4cf6a52253e2794a1d55279e7be44f5
PULKIT CHANDRA <chandra.pulkit1997@gmail.com>
2021-03-07 00:49:16 +0530

    Initial commit

---
+++ src/main/java/com/pulkit/TEST/App.java
@@ -0,0 +1,14 @@
+package com.pulkit.TEST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println("STARTING TEST APPLICATION");
+    	SpringApplication.run(App.class, args);
+    }
+}
