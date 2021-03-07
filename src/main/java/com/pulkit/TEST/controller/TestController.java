ed439dff069a7d3c3592e2612ab747e2e97dce58
PULKIT CHANDRA <chandra.pulkit1997@gmail.com>
2021-03-07 11:42:21 +0530

    2nd test

--- src/main/java/com/pulkit/TEST/controller/TestController.java
+++ src/main/java/com/pulkit/TEST/controller/TestController.java
@@ -1,12 +1,19 @@
 package com.pulkit.TEST.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 	@RequestMapping("/test")
-	public String home() {
+	public String test() {
 		return "Welcome to springboot test";
 	}
+	
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
 }
