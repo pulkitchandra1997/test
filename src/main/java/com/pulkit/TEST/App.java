eb5d7880021cbb46367ac3659ef2ca6e54e8e253
PULKIT CHANDRA <chandra.pulkit1997@gmail.com>
2021-03-07 13:18:08 +0530

    change

--- src/main/java/com/pulkit/TEST/App.java
+++ src/main/java/com/pulkit/TEST/App.java
@@ -2,9 +2,10 @@ package com.pulkit.TEST;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App 
+public class App extends SpringBootServletInitializer
 {
     public static void main( String[] args )
     {
